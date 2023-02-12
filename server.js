f4b02dede7ba5afd0b0a63c29096657ea73be970
muditgupta68 <muditgupta68@gmail.com>
2023-02-12 19:35:53 +0530

    updated builds

--- server.js
+++ server.js
@@ -4,25 +4,31 @@ const morgan = require("morgan");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const dbConnect = require("./db");
-var cookieParser = require('cookie-parser')
+var cookieParser = require("cookie-parser");
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
+const path = require("path");
 
 // routes
 const UserRouter = require("./routes/userRoutes");
 const adminRouter = require("./routes/adminRoutes");
 
-
 // middleware
 app.use(cors());
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 app.use(morgan("dev"));
 
+//static file using
+app.use(express.static(path.join(__dirname, "./client/build")));
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
 // applying routes
-app.use("/api/v1/user",UserRouter);
-app.use("/api/v1/admin",adminRouter);
+app.use("/api/v1/user", UserRouter);
+app.use("/api/v1/admin", adminRouter);
 
 //server call
 const start = async () => {
